0892ac264db59a101499a6d52340ff29b314308e
Bakhtiyar Garashov <bakhtiyar.garashov@finceptiv.com>
2021-07-27 15:41:22 +0300

    Model with point geom add

--- tbilisiConnect/settings.py
+++ tbilisiConnect/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'src'
 ]
 
@@ -75,9 +76,13 @@ WSGI_APPLICATION = 'tbilisiConnect.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "HOST": "localhost",
+        "NAME": "test",
+        "PASSWORD": "admin",
+        "PORT": 5432,
+        "USER": "postgres",
     }
 }
 
