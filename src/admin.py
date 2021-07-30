0a8f6516fb4e36121a8f2e1df1a8198d3548db32
Bakhtiyar Garashov <bakhtiyar.garashov@finceptiv.com>
2021-07-30 23:12:41 +0300

    Add more fields and adjust verbose field names

--- src/admin.py
+++ src/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from .models import Objects
+from .models import Restaurants, Tag
 from leaflet.admin import LeafletGeoAdmin
 
 # Register your models here.
+class RestaurantsAdmin(LeafletGeoAdmin, admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
 
-class ObjectsAdmin(LeafletGeoAdmin, admin.ModelAdmin):
-    list_display=('name',)
-    ordering =('name',)
-    list_filter=('name',)
-    search_fields=('name',)
 
-admin.site.register(Objects,ObjectsAdmin)
+admin.site.site_header = 'Tbilisi Connect'
+admin.site.index_title = 'Tbilisi Connect adminstration'
+admin.site.site_title = 'Tbilisi Connect adminstration'
+admin.site.register(Restaurants, RestaurantsAdmin)
+admin.site.register(Tag)
--- src/models.py
+++ src/models.py
@@ -2,18 +2,35 @@ from django.contrib.gis.db import models
 
 # Create your models here.
 
-class Objects(models.Model):
-    name = models.CharField(max_length=50)
-    description_en = models.TextField(blank=True,null=True)
-    description_ge = models.TextField(blank=True, null=True)
-    description_ru = models.TextField(blank=True, null=True)
-    location = models.PointField(srid=4326)
+
+class Tag(models.Model):
+    text = models.CharField(max_length=50)
 
     class Meta:
-        ordering=['name']
+        ordering = ['text']
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class Restaurants(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Name')
+    address = models.CharField(max_length=255, verbose_name='Address')
+    image = models.ImageField(upload_to='images', verbose_name='Image')
+    tags = models.ManyToManyField(Tag)
+    url_main = models.URLField(null=True, blank=True, verbose_name='Main url (e.g., website url)')
+    url_facebook = models.URLField(null=True, blank=True, verbose_name='Facebook page')
+    url_instagram = models.URLField(null=True, blank=True, verbose_name='Instagram page')
+    url_twitter = models.URLField(null=True, blank=True, verbose_name='Twitter account')
+    description_en = models.TextField(blank=True, null=True, verbose_name='Description (EN)')
+    description_ge = models.TextField(blank=True, null=True, verbose_name='Description (GE)')
+    description_ru = models.TextField(blank=True, null=True, verbose_name='Description (RU)')
+    location = models.PointField(srid=4326,verbose_name='Location on map')
+
+    class Meta:
+        ordering = ['name']
         verbose_name = 'Restaurant'
         verbose_name_plural = 'Restaurants'
-
 
     def __str__(self) -> str:
         return f"{self.name}"
